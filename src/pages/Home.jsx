17b14818b3934049046feb29e4065e185c6cee3a
Aryan <aryanpanchal1702@gmail.com>
2025-08-21 01:15:57 -0400

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,19 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+import { Background } from "../components/Background";
+
 export const Home = () => {
-    return <div className="home-screen">
-        
+    return <div className="home-screen bg-background text-foreground overflow-x-hidden">
         {/* Theme Toggle */}
+        <ThemeToggle/>
 
         {/* Background Effects */}
-
-
+        <Background/>
 
         {/* Navbar */}
 
+
         {/* Main Content */}
+
 
         {/* Footer */}
     </div>;
