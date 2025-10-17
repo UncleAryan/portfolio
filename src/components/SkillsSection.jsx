375c19ee9168fdfb8d1f16df0924f2bb48951530
Aryan <aryanpanchal1702@gmail.com>
2025-10-17 16:26:28 -0400

    Finished SkillsSection, started ProjectsSection

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -2,25 +2,29 @@ import { useState } from "react";
 import { cn } from "../lib/utils";
 
 const skills = [
-    {name: "HTML", level: 95, category: "frontend"},
-    {name: "Vanilla CSS", level: 95, category: "frontend"},
-    {name: "JavaScript", level: 90, category: "frontend"},
-    {name: "React", level: 70, category: "frontend"},
-    {name: "Tailwind CSS", level: 90, category: "frontend"},
-    {name: "Next.js", level: 0, category: "frontend"},
+    {name: "HTML", level: 95, category: "core-languages"},
+    {name: "Vanilla CSS", level: 95, category: "core-languages"},
+    {name: "JavaScript", level: 90, category: "core-languages"},
+    {name: "Java", level: 100, category: "core-languages"},
+    {name: "C++", level: 95, category: "core-languages"},
+    {name: "C", level: 90, category: "core-languages"},
+    {name: "Python", level: 100, category: "core-languages"},
+    {name: "Assembly", level: 85, category: "core-languages"},
+    {name: "C++", level: 95, category: "core-languages"},
+    {name: "C#", level: 95, category: "core-languages"},
+    {name: "PHP", level: 85, category: "core-languages"},
 
-    {name: "Node.js", level: 70, category: "backend"},
-    {name: "Express", level: 0, category: "backend"},
-    {name: "MongoDB", level: 20, category: "backend"},
-    {name: "PostgreSQL", level: 0, category: "backend"},
-    {name: "GraphQL", level: 0, category: "backend"},
+    {name: "React", level: 80, category: "frameworks"},
+    {name: "Tailwind CSS", level: 90, category: "frameworks"},
 
-    {name: "Git/GitHub", level: 92, category: "tools"},
+    {name: "Git/GitHub", level: 90, category: "tools"},
     {name: "Docker", level: 20, category: "tools"},
-    {name: "VS Code", level: 98, category: "tools"}
+    {name: "VS Code", level: 100, category: "tools"},
+    {name: "Visual Studio", level: 95, category: "tools"},
+    {name: "Eclipse IDE", level: 100, category: "tools"},
 ];
 
-const category = ["all", "frontend", "backend", "tools"];
+const categories = ["all", "core-languages", "frameworks", "tools"];
 
 export const SkillsSection = () => {
     const [activeCategory, setActiveCategory] = useState("all");
@@ -34,7 +38,38 @@ export const SkillsSection = () => {
                     My <span className={cn("text-primary")}>Skills</span>
                 </h2>
 
-                
+                <div className="flex flex-wrap justify-center gap-4 mb-12">
+                    {categories.map((category, key) => (
+                        <button key={key} 
+                                onClick={() => setActiveCategory(category)}
+                                className={cn("px-5 py-2 rounded-full transition-colors duration-300 capitalize",
+                                activeCategory === category ? "bg-primary text-primary-foreground" : "bg-secondary/70 text-foreground hover:bd-secondary")}>
+                                    {category}
+                        </button>
+                    ))}
+                </div>
+
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                    {filteredSkills.map((skill, key) => (
+                        <div key={key}
+                             className="bg-card p-6 rounded-lg shadow-xs card-hover">
+                            <div className="text-left mb-4">
+                                <h3 className="font-semibold text-lg"> {skill.name} </h3>
+                            </div>
+                            
+                            <div className="w-full bg-secondary/50 h-2 rounded-full overflow-hidden">
+                                <div style={{ width: skill.level + "%"}}
+                                     className="bg-primary h-2 rounded-full origin-left animate-[grow_2s_ease-out]"/>
+                            </div>
+
+                            <div className="text-right mt-1">
+                                <span className="text-sm text-muted-foreground">
+                                    {skill.level}%
+                                </span>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </section>
     );
