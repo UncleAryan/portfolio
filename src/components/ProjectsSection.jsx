d1de5052b060570136f2af9b2d8f7d05ec51c977
Aryan <aryanpanchal1702@gmail.com>
2025-12-19 19:52:56 -0500

    updated how CV and images are accessed with dark theme as default

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,13 +1,12 @@
 import { ArrowRight, Github } from "lucide-react";
 
-const BASE_URL = import.meta.env.BASE_URL || '/';
 
 const projects = [
     {
         id: 1,
         title: "Homie Hand",
         description: "A 2D game engine made entirely in Java from scratch with custom hand-drawn spritesheets.",
-        image: `${BASE_URL}project_screenshots/homiehand_gameplay.png`,
+        image: "/portfolio/project_screenshots/homiehand_gameplay.png",
         tags: ["Java", "Object-Oriented Programming", "Game Loop", "Entity Management", "Input Handling", "Sprite Animation", "Scene Management", "Collision Detection System"],
         github_url: "https://github.com/UncleAryan/HomieHand"
     },
@@ -15,7 +14,7 @@ const projects = [
         id: 2,
         title: "Custom 3D Rendering Pipeline with Unity",
         description: "A custom-built 3D rendering pipeline implemented from scratch in Unity3D, demonstrating fundamental computer graphics concepts including matrix transformations, projection, and rasterization using only basic OpenGL line drawing.",
-        image: `${BASE_URL}project_screenshots/custom_3d_rendering_pipeline_example.png`,
+        image: "/portfolio/project_screenshots/custom_3d_rendering_pipeline_example.png",
         tags: ["C#", "Unity Game Engine", "Linear Algebra", "Agile Methodologies", "Version Control"],
         github_url: "https://github.com/UncleAryan/custom-3d-rendering-pipeline"
     },
@@ -23,7 +22,7 @@ const projects = [
         id: 3,
         title: "Autonomous Snake Game",
         description: "Classic Snake game that plays itself using implementation of A* pathfinding to calculate shortest path to food while dynamically avoiding collisions in real time.",
-        image: `${BASE_URL}project_screenshots/snake_ai_example.png`,
+        image: "/portfolio/project_screenshots/snake_ai_example.png",
         tags: ["C#", "Unity Game Engine", "Advanced Data Structures", "Artifical Intelligence through Heuristics"],
         github_url: "https://github.com/UncleAryan/Snake-AI"
     }
@@ -53,7 +52,7 @@ export const ProjectsSection = () => {
                             {project.title}
                         </h3>
 
-                        <div className="h-90 w-full overflow-hidden">
+                        <div className="h-90 w-full overflow-hidden bg-muted">
                             <img src={project.image} 
                                  alt={project.title}
                                  className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105" />
